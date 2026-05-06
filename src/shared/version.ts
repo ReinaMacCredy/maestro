df764a7ad50621ac3ecf58756bbf40309f1e4328
ReinaMacCredy <reina.maccredy@outlook.com>
2026-05-07 01:14:44 +0700

    chore(release): v0.72.40

--- src/shared/version.ts
+++ src/shared/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = "0.72.39";
-export const BUILD_UNIX = 1778090793;
-export const GIT_SHA = "1dc3ecd";
-export const RELEASED_AT = "2026-05-06T18:06:33.667Z";
+export const VERSION = "0.72.40";
+export const BUILD_UNIX = 1778091284;
+export const GIT_SHA = "258aa46";
+export const RELEASED_AT = "2026-05-06T18:14:44.246Z";
