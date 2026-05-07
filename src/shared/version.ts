43e0d9d8bb8b0e2bb3cd7f31d84f153d08747e34
ReinaMacCredy <reina.maccredy@outlook.com>
2026-05-07 18:41:30 +0700

    chore(release): bump version to 0.75.0

--- src/shared/version.ts
+++ src/shared/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = "0.74.1";
-export const BUILD_UNIX = 1778129545;
-export const GIT_SHA = "e9a94f2";
-export const RELEASED_AT = "2026-05-07T04:52:25.978Z";
+export const VERSION = "0.75.0";
+export const BUILD_UNIX = 1778151837;
+export const GIT_SHA = "f69ebd5";
+export const RELEASED_AT = "2026-05-07T11:03:57.562Z";
